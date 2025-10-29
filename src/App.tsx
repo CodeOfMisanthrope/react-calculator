d410ac6aadf58e08c7614071af525614ad531a2b
Ilya <iosdsbb@gmail.com>
2025-10-29 09:58:50 +0300

    feat: updated App, added handlers and btn ops

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,40 @@
+import {type MouseEvent} from "react";
 import {useState} from 'react';
 import style from '~/app.module.css';
 
 function App() {
     const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+    const [expression, setExpression] = useState([]);
+
+    const onClickNum = (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+        const num = Number(event.currentTarget.dataset.num);
+        console.log(num);
+    };
+
+    const onClickSum = (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    }
+
+    const onClickDiff = (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
+    const onClickResult = (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
+    const onClickClear = (event: MouseEvent<HTMLButtonElement>) => {
+        event.preventDefault();
+    };
+
+    const operations = [
+        {op: '+', handler: onClickSum},
+        {op: '-', handler: onClickDiff},
+        {op: '=', handler: onClickResult},
+        {op: 'c', handler: onClickClear},
+    ];
 
     return (
         <>
@@ -11,10 +43,27 @@ function App() {
                 {/*<label htmlFor="input-number" className={style['calc__label']}>Введите выражение</label>*/}
                 {/*<input id="input-number" className={style['calc__input-number'] + ' ' + 'calc-input'} type="number"/>*/}
                 <div className={style['calc__display']}></div>
-                <div className={style['calc__list']}>
+                <div className={style['calc__list-operation']}>
+                    {operations.map(({op, handler}) => (
+                        <div className={style['calc__item-operation']}>
+                            <button
+                                className={style['calc__btn-operation'] + ' ' + 'calc-btn'}
+                                onClick={handler}
+                                data-op={op}>
+                                {op}
+                            </button>
+                        </div>
+                    ))}
+                </div>
+                <div className={style['calc__list-num']}>
                     {nums.map((num) =>
-                        <div className={style['calc__item']} key={num}>
-                            <button className={style['calc__btn-num'] + ' ' + 'calc-btn'}>{num}</button>
+                        <div className={style['calc__item-num']} key={num}>
+                            <button
+                                className={style['calc__btn-num'] + ' ' + 'calc-btn'}
+                                onClick={onClickNum}
+                                data-num={num}>
+                                {num}
+                            </button>
                         </div>
                     )}
                 </div>

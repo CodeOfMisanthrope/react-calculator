f8bfb119fbae739856b351d9d9357ceb56022de9
Ilya <iosdsbb@gmail.com>
2025-10-29 08:05:33 +0300

    feat: updated App

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
-import { useState } from 'react';
+import {useState} from 'react';
 import style from '~/app.module.css';
+// import styleMain from '~assets/styles/main.css';
 
 function App() {
-  return (
-    <>
-      <h1>Калькулятор</h1>
-    </>
-  )
+    const nums = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+
+    return (
+        <>
+            <h1 className={style['calc__title']}>Калькулятор</h1>
+            <form className={style['calc__list']} action="">
+                <label htmlFor="input-number" className={style['calc__label']}>Введите выражение</label>
+                <input id="input-number" className={style['calc__input-number'] + ' ' + 'input-number'} type="number"/>
+                <div className={style['calc__list']}>
+                    {nums.map((num) =>
+                        <div className={style['calc__item']} key={num}>
+                            <button className={style['calc__btn']}>{num}</button>
+                        </div>
+                    )}
+                </div>
+            </form>
+        </>
+    );
 }
 
 export default App;
